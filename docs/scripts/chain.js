b8a033f73e3772765228e5ca6eca89def49462fd
dluman <dluman@allegheny.edu>
2022-04-25 06:02:06 -0400

    Adding Web3 functionality

---
+++ docs/scripts/chain.js
@@ -0,0 +1,41 @@
+const address = "0x81855562a4388B2c76A58B06884E834d67C03B06";
+const abi = [
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_status",
+        "type": "string"
+      }
+    ],
+    "name": "setStatus",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getStatus",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+const web3Instance = async() => {
+  if (window.ethereum) {
+    const web3 = new Web3(window.ethereum);
+    await window.etherem.enable();
+    const wallet = window.ethereum.selectedAddress;
+    const statusContract = web3.eth.Contract(abi, address);
+    contract.methods.getStatus().send({
+      from: wallet
+    });
+ }
+}
